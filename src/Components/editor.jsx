d57a9f246f00abf878de9a653646565d31a27cf6
KangHaSeong <shaawwert6044@gmail.com>
2023-04-13 16:57:32 +0900

    react-quill의 낮은 확장성으로 인해 react-draft 사용

--- src/Components/editor.jsx
+++ src/Components/editor.jsx
@@ -1,68 +1,8 @@
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-const toolbarOptions = [
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
-  ['bold', 'italic', 'underline', 'strike'], // text styles
-  ['blockquote', 'code-block'], // blocks
-  [{ list: 'ordered' }, { list: 'bullet' }],
-  [{ indent: '-1' }, { indent: '+1' }], // indentation
-  [{ align: [] }],
-  [{ color: [] }, { background: [] }], // colors
-  ['link', 'image', 'video'], // links and media
-  ['clean'], // remove formatting
-];
-
-const modules = {
-  toolbar: {
-    container: toolbarOptions,
-    handlers: {
-      'code-block': () => {
-        const range = this.quill.getSelection();
-        this.quill.format('code-block', true);
-        if (range) {
-          this.quill.setSelection(range.index + 1, range.length);
-        }
-      },
-      'horizontal-line': () => {
-        const range = this.quill.getSelection();
-        this.quill.insertEmbed(range.index, 'hr', null, 'user');
-        this.quill.setSelection(range.index + 2, 0);
-      },
-      'insert-table': () => {
-        const range = this.quill.getSelection();
-        this.quill.insertTable(3, 3);
-        this.quill.setSelection(range.index + 4, 0);
-      },
-      'insert-tag': () => {
-        const range = this.quill.getSelection();
-        this.quill.insertText(range.index, 'tag');
-        this.quill.setSelection(range.index + 4, 0);
-      },
-      'delete-backward': (range, context) => {
-        const currentIndex = range.index;
-        const previousCharIndex = currentIndex - 1;
-        const previousChar = context.lines.current[previousCharIndex];
-        const nextChar = context.lines.current[currentIndex];
-        if (previousChar === '"' && nextChar === '"') {
-          context.quill.deleteText(previousCharIndex, 2, 'user');
-          context.quill.setSelection(previousCharIndex, 0, 'user');
-        } else {
-          context.quill.deleteText(currentIndex, 1, 'user');
-        }
-      },
-    },
-  },
-};
-
-const Editor = () => {
-  const handleChangeForLook = (value) => {
-    console.log(value);
-  };
-
+const Edior = () => {
   return (
-    <ReactQuill theme="snow" modules={modules} onChange={handleChangeForLook} />
-  );
-};
-
-export default Editor;
+    <Editor />
+  )
+}
