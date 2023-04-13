0df0488318c8f99e39d0f54398efd14196bf61a7
KangHaSeong <shaawwert6044@gmail.com>
2023-04-13 14:12:20 +0900

    퀼 임포트 및 eslintrc/prettierrc 설정

---
+++ src/Components/editor.jsx
@@ -0,0 +1,10 @@
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const Editor = () => {
+  return <>
+    <ReactQuill theme="snow" />
+  </>;
+};
+
+export default Editor;
