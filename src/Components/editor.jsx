4504833e748ffcb9f7dde3033c0c11bf30ec5a0f
KangHaSeong <shaawwert6044@gmail.com>
2023-04-13 16:04:32 +0900

    툴바 셋팅 완료, 코드블럭 에러 및 인라인 블럭 처리 중

--- src/Components/editor.jsx
+++ src/Components/editor.jsx
@@ -1,10 +1,68 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+const toolbarOptions = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  ['bold', 'italic', 'underline', 'strike'], // text styles
+  ['blockquote', 'code-block'], // blocks
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  [{ indent: '-1' }, { indent: '+1' }], // indentation
+  [{ align: [] }],
+  [{ color: [] }, { background: [] }], // colors
+  ['link', 'image', 'video'], // links and media
+  ['clean'], // remove formatting
+];
+
+const modules = {
+  toolbar: {
+    container: toolbarOptions,
+    handlers: {
+      'code-block': () => {
+        const range = this.quill.getSelection();
+        this.quill.format('code-block', true);
+        if (range) {
+          this.quill.setSelection(range.index + 1, range.length);
+        }
+      },
+      'horizontal-line': () => {
+        const range = this.quill.getSelection();
+        this.quill.insertEmbed(range.index, 'hr', null, 'user');
+        this.quill.setSelection(range.index + 2, 0);
+      },
+      'insert-table': () => {
+        const range = this.quill.getSelection();
+        this.quill.insertTable(3, 3);
+        this.quill.setSelection(range.index + 4, 0);
+      },
+      'insert-tag': () => {
+        const range = this.quill.getSelection();
+        this.quill.insertText(range.index, 'tag');
+        this.quill.setSelection(range.index + 4, 0);
+      },
+      'delete-backward': (range, context) => {
+        const currentIndex = range.index;
+        const previousCharIndex = currentIndex - 1;
+        const previousChar = context.lines.current[previousCharIndex];
+        const nextChar = context.lines.current[currentIndex];
+        if (previousChar === '"' && nextChar === '"') {
+          context.quill.deleteText(previousCharIndex, 2, 'user');
+          context.quill.setSelection(previousCharIndex, 0, 'user');
+        } else {
+          context.quill.deleteText(currentIndex, 1, 'user');
+        }
+      },
+    },
+  },
+};
+
 const Editor = () => {
-  return <>
-    <ReactQuill theme="snow" />
-  </>;
+  const handleChangeForLook = (value) => {
+    console.log(value);
+  };
+
+  return (
+    <ReactQuill theme="snow" modules={modules} onChange={handleChangeForLook} />
+  );
 };
 
 export default Editor;
