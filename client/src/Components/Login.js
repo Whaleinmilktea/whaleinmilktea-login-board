0dc35b9e9885846a343cc9e502a46fb26fbf1723
KangHaSeong <shaawwert6044@gmail.com>
2023-04-18 09:15:39 +0900

    세션 로그인 구현 완료

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import styled from 'styled-components';
 
 const LoginWrapper = styled.div`
@@ -41,12 +44,57 @@ const LoginBtn = styled.button`
 `;
 
 const Login = () => {
+  const [id, setId] = useState('');
+  const [ps, setPs] = useState('');
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    // 서버로부터 세션 정보를 가져오는 요청을 수행하는 함수
+    const checkLoginStatus = async () => {
+      try {
+        const res = await axios.get('/checkLoginStatus');
+        const isLoggedIn = res.data.isLoggedIn;
+
+        if (isLoggedIn) {
+          // 로그인이 되어있는 경우에는 메인 페이지로 이동
+          window.location.href = '/main';
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    checkLoginStatus();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/login', { id, ps });
+      setMsg(res.data.msg);
+      alert("로그인 성공")
+    } catch (err) {
+      setMsg(err.res.data.msg);
+      alert("로그인 실패")
+    }
+  };
+
   return (
     <LoginWrapper>
-      <LoginForm>
-        <IdInput type="text" placeholder="아이디를 입력하세요" />
-        <PsInput type="password" placeholder="비밀번호를 입력하세요" />
+      <LoginForm onSubmit={handleSubmit}>
+        <IdInput
+          type="text"
+          placeholder="아이디를 입력하세요"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <PsInput
+          type="password"
+          placeholder="비밀번호를 입력하세요"
+          value={ps}
+          onChange={(e) => setPs(e.target.value)}
+        />
         <LoginBtn>로그인</LoginBtn>
+        {msg && <p>{msg}</p>}
       </LoginForm>
     </LoginWrapper>
   );
